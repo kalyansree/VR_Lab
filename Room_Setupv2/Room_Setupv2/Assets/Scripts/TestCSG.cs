631a7873f83538c363a28fdc20e820baba0d9616
Kazuhiro Uchikata <kazuuchikata@gmail.com>
2017-06-24 16:59:10 -0500

    got testCSG scene mostly working

--- Room_Setupv2/Room_Setupv2/Assets/Scripts/TestCSG.cs
+++ Room_Setupv2/Room_Setupv2/Assets/Scripts/TestCSG.cs
@@ -9,7 +9,6 @@ public class TestCSG : MonoBehaviour {
 // Initialize two new meshes in the scene
 public GameObject Sphere1;
 public GameObject Sphere2;
-public GameObject Sphere3;
 
 
 
@@ -23,15 +22,25 @@ public GameObject Sphere3;
         composite.AddComponent<MeshFilter>().sharedMesh = m;
         composite.AddComponent<MeshRenderer>().sharedMaterial = Sphere1.GetComponent<MeshRenderer>().material;
         composite.transform.position = Sphere1.transform.position;
+
+        GameObject composite2 = new GameObject("Hemisphere");
+        composite2.AddComponent<MeshFilter>().sharedMesh = m;
+        composite2.AddComponent<MeshRenderer>().sharedMaterial = Sphere1.GetComponent<MeshRenderer>().material;
+        composite2.transform.position = Sphere1.transform.position;
+        var rotationVector = composite2.transform.rotation.eulerAngles;
+        rotationVector.y += 60;
+        composite2.transform.rotation = Quaternion.Euler(rotationVector);
+        composite2.transform.localScale = composite2.transform.localScale * 1.3F;
         Sphere1.SetActive(false);
         Sphere2.SetActive(false);
 
-        Mesh n = CSG.Intersect(composite, Sphere3);
+        Mesh n = CSG.Intersect(composite, composite2);
         GameObject final = new GameObject("Final");
         final.AddComponent<MeshFilter>().sharedMesh = n;
         final.AddComponent<MeshRenderer>().sharedMaterial = composite.GetComponent<MeshRenderer>().material;
         final.transform.position = composite.transform.position;
         composite.SetActive(false);
+        composite2.SetActive(false);
         //GameObject prefab = PrefabUtility.CreatePrefab("Assets/Meshes/Hemisphere.prefab", composite, ReplacePrefabOptions.ReplaceNameBased);
         //SaveMesh(composite.GetComponent<MeshFilter>().mesh, "HemisphereFinal");
         //Object prefab = PrefabUtility.CreateEmptyPrefab("Assets/Meshes/hemisphere.prefab");
