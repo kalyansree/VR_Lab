542183c62672180af3ae16526d3d4b7acb56c315
Kazuhiro Uchikata <kazuuchikata@gmail.com>
2017-07-28 20:09:45 -0500

    made progress on csg

--- Room_Setupv2/Room_Setupv2/Assets/Scripts/TestCSG.cs
+++ Room_Setupv2/Room_Setupv2/Assets/Scripts/TestCSG.cs
@@ -2,25 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Parabox.CSG;
+using Vectrosity;
+
 
 public class TestCSG : MonoBehaviour {
+    private GameObject hemisphere1;
+    private GameObject hemisphere2;
 
+    private GameObject final;
 
-    
+    public Material hemisphereMaterial;
 
+    private VectorLine hemisphereLine;
+
+    private List<Transform> sphereList;
+    public Camera myCamera;
+    public GameObject target;
     void Start () {
-        Vector3 RotationVec = new Vector3(0, 60, 0);
+        sphereList = new List<Transform>();
+        Vector3 RotationVec = new Vector3(0,1,0);
         Vector3 scale = new Vector3(1, 1, 1);
         Vector3 position = new Vector3(0, 0, 0);
-        GameObject composite = CreateHemisphere();
-        //composite.SetActive(false);
-        GameObject composite2 = CreateHemisphere(Rotation: Quaternion.Euler(RotationVec));
+        VectorLine.SetCamera3D(myCamera);
+        hemisphereLine = new VectorLine("HemiLine", new List<Vector3>(), 2.0f, LineType.Discrete);
+        hemisphereLine.Draw3DAuto();
+        hemisphere1 = CreateHemisphere();
+        //DrawHemisphereLines(hemisphere1, "hemisphere");
+        hemisphere1.SetActive(false);
+        hemisphere2 = CreateHemisphere(Rotation: Quaternion.LookRotation(RotationVec,  RotationVec));
         //composite.SetActive(true);
-        GameObject final = GetIntersection(composite, composite2);
+        //hemisphere2.SetActive(false);
+        print(LayerMask.NameToLayer("hemisphere"));
 
-        //composite.SetActive(false);
-        //composite2.SetActive(false);
 
+  
+
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            final = GetIntersection(hemisphere1, hemisphere2);
+            //final = GetIntersection(Hemisphere, Hemisphere2);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            DrawHemisphereLines(final, "final");
+        }
+        int i = 0;
+        foreach (Transform transform in sphereList)
+        {
+            hemisphereLine.points3[i] = sphereList[i].position;
+            i++;
+        }
+
+        
+        hemisphere2.transform.rotation = Quaternion.LookRotation(target.transform.position);
+        Vector3 euler = hemisphere2.transform.rotation.eulerAngles;
+        hemisphere2.transform.eulerAngles = new Vector3(euler.z, euler.y + 90F, euler.x);
+        
 
     }
 
@@ -28,60 +68,57 @@ public class TestCSG : MonoBehaviour {
     {
         GameObject Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Cube.transform.localScale = new Vector3(2, 2, 2);
         GameObject Pivot = new GameObject();
         Sphere.transform.position = Position;
-        Cube.transform.position = Position + new Vector3(0.5F,0,0);
+        Cube.transform.position = Position + new Vector3(1,0,0);
         Pivot.transform.position = Position;
         Sphere.transform.parent = Pivot.transform;
         Cube.transform.parent = Pivot.transform;
-        Pivot.transform.rotation = Rotation;
+        //Pivot.transform.rotation = Rotation;
 
         
         Mesh m = CSG.Intersect(Sphere, Cube);
         m.name = "Hemisphere Mesh";
         GameObject composite = new GameObject("Hemisphere");
         composite.AddComponent<MeshFilter>().sharedMesh = m;
-        composite.AddComponent<MeshRenderer>().sharedMaterial = Sphere.GetComponent<MeshRenderer>().material;
+        composite.AddComponent<MeshRenderer>().sharedMaterial = hemisphereMaterial;
         if (Scale == null)
             composite.transform.localScale = new Vector3(1, 1, 1);
         else
             composite.transform.localScale = (Vector3)Scale;
-        Destroy(Cube);
         composite.AddComponent<MeshCollider>();
         composite.GetComponent<MeshCollider>().sharedMesh = m;
-        //createChildSpheres(Sphere, composite);
+        composite.layer = LayerMask.NameToLayer("hemisphere");
+        composite.transform.rotation = Rotation;
         Destroy(Sphere);
-        
         Destroy(Pivot);
-
+        Destroy(Cube);
         return composite;
     }
 
     public GameObject GetIntersection(GameObject Hemi1, GameObject Hemi2, Vector3 Position = default(Vector3), Vector3? Scale = null)
     {
-        GameObject Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        Sphere.transform.position = Position;
-        if(Scale == null)
-            Sphere.transform.localScale = new Vector3(1, 1, 1);
-        else
-            Sphere.transform.localScale = (Vector3)Scale;
+        
         if (Hemi1.transform.localScale == Hemi2.transform.localScale)
         {
-            Hemi1.transform.localScale *= 1.3F;
+            Hemi1.transform.localScale *= 2F;
         }
         Mesh n = CSG.Intersect(Hemi1, Hemi2);
+        n.name = "Truncated Sphere";
         GameObject final = new GameObject("Final");
         final.AddComponent<MeshFilter>().sharedMesh = n;
-        final.AddComponent<MeshRenderer>().sharedMaterial = Hemi1.GetComponent<MeshRenderer>().material;
+        final.AddComponent<MeshRenderer>().sharedMaterial = hemisphereMaterial;
+        final.AddComponent<MeshCollider>();
+        final.GetComponent<MeshCollider>().sharedMesh = n;
+        final.layer = LayerMask.NameToLayer("final");
         Destroy(Hemi1);
         Destroy(Hemi2);
-        createChildSpheres(Sphere, final);
-        Destroy(Sphere);
 
         return final;
     }
 
-    public void createChildSpheres(GameObject origin, GameObject hemisphere)
+    public List<GameObject> createChildSpheres(GameObject origin, GameObject hemisphere, string layerName)
     {
         float scaling = origin.transform.localScale.x / 2;
         int numSpheres = 128;
@@ -95,14 +132,10 @@ public class TestCSG : MonoBehaviour {
             uspheres[i].transform.position = origin.transform.TransformPoint(value * scaling);
             float scale = scaling / numSpheres * 2;
             uspheres[i].transform.localScale = new Vector3(scale, scale, scale);
-            //Vector3 closestPoint = hemisphere.GetComponent<MeshCollider>().ClosestPoint(uspheres[i].transform.position);
-            //var distToParent = Vector3.Distance(closestPoint, uspheres[i].transform.position);
-            //if (distToParent > 0)
-            //{
-            //    uspheres[i].SetActive(false);
-            //}
-            Collider[] hitColliders = Physics.OverlapSphere(uspheres[i].transform.position, scale / 2);
-            if (hitColliders.Length <= 6)
+            int layerMask = 1 << LayerMask.NameToLayer(layerName);
+            Collider[] hitColliders = Physics.OverlapSphere(uspheres[i].transform.position, scale / 2, layerMask);
+
+            if (hitColliders.Length == 0)
             {
                 GameObject obj = uspheres[i];
                 uspheres.Remove(obj);
@@ -110,11 +143,13 @@ public class TestCSG : MonoBehaviour {
             }
             else
             {
-                //uspheres[i].transform.parent = hemisphere.transform;
+                uspheres[i].transform.parent = hemisphere.transform;
+                uspheres[i].SetActive(false);
                 i++;
             }
 
         }
+        return uspheres;
     }
 
     Vector3[] PointsOnSphere(int n)
@@ -140,5 +175,32 @@ public class TestCSG : MonoBehaviour {
         }
         Vector3[] pts = upts.ToArray();
         return pts;
+    }
+
+    private void DrawHemisphereLines(GameObject hemisphere, string layerName, Vector3 Position = default(Vector3), Vector3? Scale = null)
+    {
+        GameObject Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Sphere.transform.position = Position;
+        if (Scale == null)
+            Sphere.transform.localScale = new Vector3(1, 1, 1);
+        else
+            Sphere.transform.localScale = (Vector3)Scale;
+
+        List<GameObject> childList = createChildSpheres(Sphere, hemisphere, layerName);
+        foreach (GameObject childSphere in childList)
+        {
+            hemisphereLine.points3.Add(Sphere.transform.position);
+            hemisphereLine.points3.Add(childSphere.transform.position);
+
+            sphereList.Add(Sphere.transform);
+            sphereList.Add(childSphere.transform);
+        }
+        hemisphereLine.color = Color.green;
+
+        //VectorLine line = new VectorLine("Wireframe", new List<Vector3>(), 1.0f, LineType.Discrete);
+        //line.MakeWireframe(((MeshFilter)hemisphere.GetComponent("MeshFilter")).mesh);
+        //line.drawTransform = hemisphere.transform;
+        //line.Draw3DAuto();
+        Sphere.SetActive(false);
     }
 }
