18067ac8e47f9154d4f43df109c770d6e5138ad1
Kazuhiro Uchikata <kazuuchikata@gmail.com>
2017-08-23 17:09:28 -0500

    Added Reset All Button

---
+++ Room_Setupv2/Room_Setupv2/Assets/Scripts/ResetAll.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ResetAll : MonoBehaviour {
+
+    public void ResetAllButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
